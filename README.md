bfaf14fc9c212401f451ed658ce141e53640dd10
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2018-07-06 17:43:45 +0200

    Hello world Spring Boot app

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# V Service" 
+# V Service
---
+++ src/main/java/me/veloxdigitis/vservice/VServiceApplication.java
@@ -0,0 +1,21 @@
+package me.veloxdigitis.vservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VServiceApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello world";
+    }
+
+}
