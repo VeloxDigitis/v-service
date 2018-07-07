94d050aca6d8b79a8d6f49dacb6e537723436f4e
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2018-07-07 18:02:10 +0200

    Add some fancy layout

--- v-service-client/src/app/app.module.ts
+++ v-service-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,6 +15,7 @@ import { AppliancesComponent } from './appliances/appliances.component';
   ],
   imports: [
     NgbModule.forRoot(),
+    FormsModule,
     BrowserModule,
     HttpClientModule
   ],
